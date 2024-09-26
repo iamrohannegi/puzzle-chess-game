03cf91e9a6229d4ec097eca3f8b2f1f814b45fd7
Rohan Negi <r.negi191998@gmail.com>
2024-09-25 23:25:08 -0300

    railway deploy with celery

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,7 +112,9 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts":  [(env('REDIS_URL', default='127.0.0.1'), 6379)],  # Replace with your Redis server address
+            "hosts": [
+                (env('REDIS_HOST', default='127.0.0.1'), int(env('REDIS_PORT', default='6379')))
+            ],
         },
     },
 }
