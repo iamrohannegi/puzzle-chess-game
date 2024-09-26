cf12d638a8b364959c6e05fdff2e147f95a6b00e
Rohan Negi <r.negi191998@gmail.com>
2024-09-25 21:58:36 -0300

    deploy to railway

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,26 +1,32 @@
 """
 ASGI config for backend project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-# from channels.auth import AuthMiddlewareStack 
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+import environ
 
+# Load environment variables from .env if it exists
+env = environ.Env()
+env_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+
+if os.path.exists(env_file):
+    environ.Env.read_env(env_file)
+
+# Set default Django settings module if not set
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Get the ASGI application
 django_asgi_app = get_asgi_application()
 
+# Now import routing and middleware after the application is ready
 from api import routing
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
+# Define the ASGI application with routing for http and websocket
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
