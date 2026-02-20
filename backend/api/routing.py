4a9068d5369bc9030b0d3123e997308acccd7534
Rohan Negi <r.negi191998@gmail.com>
2026-02-20 11:21:19 +0530

    Update routing.py

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .consumers import *
 
 websocket_urlpatterns = [
-    path("ws/gameroom/<gameroom_name>/", GameRoomConsumer.as_asgi()),    
-    path("ws/lobby/", LobbyConsumer.as_asgi()),
+    path("ws/gameroom/<gameroom_name>", GameRoomConsumer.as_asgi()),    
+    path("ws/lobby", LobbyConsumer.as_asgi()),
 ]
